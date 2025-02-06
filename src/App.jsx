3d8708a1976f04b12278904a83416290ed91245a
AkshayKumar20043 <ak5559338@gmail.com>
2025-02-06 12:01:27 +0530

    nw

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello React Application Deploy on EC2 with AWS</h1>
+    </div>
+  )
+}
+
+export default App
